3b9744b352918907cb32fa62ba6ae7e68363b026
biwoom <bingeoul@gmail.com>
2017-10-13 05:39:43 +0000

    markitup edit page button auto markdown input text change

--- wiki_site/static/wiki/markitup/sets/frontend/set.js
+++ wiki_site/static/wiki/markitup/sets/frontend/set.js
@@ -13,13 +13,16 @@
 mySettings = {
 	previewParserPath:	'',
 	onShiftEnter:		{keepDefault:false, openWith:'\n\n'},
+	// kjh
 	markupSet: [
-		{name:'First Level Heading', key:'1', placeHolder:'Your title here...', closeWith:function(markItUp) { return miu.markdownTitle(markItUp, '=') } },
-		{name:'Second Level Heading', key:'2', placeHolder:'Your title here...', closeWith:function(markItUp) { return miu.markdownTitle(markItUp, '-') } },
-		{name:'Heading 3', key:'3', openWith:'### ', placeHolder:'Your title here...' },
-		{name:'Heading 4', key:'4', openWith:'#### ', placeHolder:'Your title here...' },
-		{name:'Heading 5', key:'5', openWith:'##### ', placeHolder:'Your title here...' },
-		{name:'Heading 6', key:'6', openWith:'###### ', placeHolder:'Your title here...' },
+		// {name:'First Level Heading', key:'1', placeHolder:'Your title here...', closeWith:function(markItUp) { return miu.markdownTitle(markItUp, '=') } },
+		// {name:'Second Level Heading', key:'2', placeHolder:'Your title here...', closeWith:function(markItUp) { return miu.markdownTitle(markItUp, '-') } },
+		{name:'First Level Heading', key:'1', openWith:'# ', placeHolder:'Your 1st title here...' },
+		{name:'Second Level Heading', key:'2', openWith:'## ', placeHolder:'Your 2nd title here...' },
+		{name:'Heading 3', key:'3', openWith:'### ', placeHolder:'Your 3rd title here...' },
+		{name:'Heading 4', key:'4', openWith:'#### ', placeHolder:'Your 4th title here...' },
+		{name:'Heading 5', key:'5', openWith:'##### ', placeHolder:'Your 5th title here...' },
+		{name:'Heading 6', key:'6', openWith:'###### ', placeHolder:'Your 6th title here...' },
 		{separator:'---------------' },
 		{name:'Bold', key:'B', openWith:'**', closeWith:'**'},
 		{name:'Italic', key:'I', openWith:'_', closeWith:'_'},
@@ -30,7 +33,8 @@ mySettings = {
 		}},
 		{separator:'---------------'},
 		{name:'Quotes', openWith:'> '},
-		{name:'Code Block / Code', openWith:'(!(    |!|`)!)', closeWith:'(!(`)!)'},
+		// {name:'Code Block / Code', openWith:'(!(    |!|`)!)', closeWith:'(!(`)!)'},
+		{name:'Code Block / Code', openWith:'```\n', placeHolder:'Your code here', closeWith:'\n``` '},
 	]
 }
 

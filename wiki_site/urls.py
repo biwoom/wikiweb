176d38190fe9f1212d17a7844838d3aa48e7b4c3
biwoom <bingeoul@gmail.com>
2017-12-25 06:53:15 +0000

    add robots.txt for google & naver

--- wiki_site/urls.py
+++ wiki_site/urls.py
@@ -8,12 +8,14 @@ from django.http.response import HttpResponse
 from django.views.static import serve as static_serve
 from django_nyt.urls import get_pattern as get_notify_pattern
 from wiki.urls import get_pattern as get_wiki_pattern
+from django.views.generic import TemplateView
 
 admin.autodiscover()
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^robots.txt', lambda _: HttpResponse('User-agent: *\nDisallow: /')),
+    # url(r'^robots.txt', lambda _: HttpResponse('User-agent: *\nDisallow: /')),
+    url(r'^robots.txt$', TemplateView.as_view(template_name="robots.txt", content_type="text/plain"), name="project_robots_file"),
     url(r'^dictapp/', include('dictapp.urls')),
     url(r'^bookapp/', include('bookapp.urls')),
     # url(r'^blogapp/', include('blogapp.urls')),

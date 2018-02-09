be162cb6389dbd6bbc9a9311a4648bb65c5a9b6c
biwoom <bingeoul@gmail.com>
2018-02-09 14:10:28 +0000

    add sidebar list & toc list to wikiapp-fix2

--- wiki/views/mixins.py
+++ wiki/views/mixins.py
@@ -39,6 +39,8 @@ class ArticleMixin(TemplateResponseMixin):
         kwargs['article'] = self.article
         kwargs['article_tabs'] = registry.get_article_tabs()
         kwargs['children_slice'] = self.children_slice[:20]
+        kwargs['children_slice_100'] = self.children_slice[:100]
         kwargs['children_slice_more'] = len(self.children_slice) > 20
+        kwargs['children_slice_more_100'] = len(self.children_slice) > 100
         kwargs['plugins'] = registry.get_plugins()
         return kwargs

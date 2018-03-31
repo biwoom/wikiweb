955500b4ddfe2dce2c1b08962d24879d12b877cc
biwoom <bingeoul@gmail.com>
2018-03-31 15:05:23 +0000

    some fix14

--- introapp/views.py
+++ introapp/views.py
@@ -64,18 +64,15 @@ def regular_donation(request):
                 if data_uri:
                     data = data_uri.decode('utf8').split(',')
                     encoded_image = data[1]
-                    # donor_name = data[2]
-                    donor_email = data[2]
-                    # encoded_image = data_uri.decode('utf8').split(',')[1]
-                    # donor_name = data_uri.decode('utf8').split(',')[2]
-                    # donor_mobile = data_uri.decode('utf8').split(',')[3]
+                    donor_name = data[2]
+                    donor_email = data[3]
                     decoded_image = base64.b64decode(encoded_image)
                     PROJECT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
                     DIRECTORY_NAME = PROJECT_DIR + '/wiki_site/media/signature/'
                     if not(os.path.isdir(DIRECTORY_NAME)):
                         os.makedirs(os.path.join(DIRECTORY_NAME))
-                    # image_name_1 = donor_name +'-'+ donor_mobile +'-'+ "signature.png"
-                    image_name_1 = donor_email + '-'+ "signature.png"
+                    image_name_1 = donor_name +'-'+ donor_email +'-'+ "signature.png"
+                    # image_name_1 = donor_email + '-'+ "signature.png"
                     image_name = image_name_1.replace(' ','-') 
                     filepath = os.path.join(DIRECTORY_NAME, image_name)
                     image_result = open(filepath, 'wb')
@@ -107,8 +104,8 @@ def regular_donation(request):
             bank_division = form.cleaned_data.get("bank_division")
             withdrawal_date = form.cleaned_data.get("withdrawal_date")
             
-            image_name_1 = to_member_email +'-'+ "signature.png"
-            # image_name_1 = real_name +'-'+ to_member_email +'-'+date+'-'+ "-signature.png"
+            # image_name_1 = to_member_email +'-'+ "signature.png"
+            image_name_1 = real_name +'-'+ to_member_email +'-'+ "-signature.png"
             image_name = image_name_1.replace(' ','-')
             signature_url = '/media/signature/' + image_name
             

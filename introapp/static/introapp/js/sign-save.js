dc4f877136f6939c8c43afdae82172d2f9dfdd53
biwoom <bingeoul@gmail.com>
2018-03-31 14:08:29 +0000

    some fix9

--- introapp/static/introapp/js/sign-save.js
+++ introapp/static/introapp/js/sign-save.js
@@ -18,10 +18,8 @@ $(function() {
     
      $('.button_save').click(function() {
             var dataURL = signaturePad.toDataURL();
-            // var donor_name = document.getElementById('id_name').value;
-            var donor_email = document.getElementById('id_email').value;
-            // dataURL = dataURL+','+donor_name+','+donor_email
-            dataURL = dataURL+','+donor_email
+            var donor_name = document.getElementById('id_name').value;
+            dataURL = dataURL+','+donor_name
             $.ajax({
                 type: 'POST',
                 url: '/regular_donation/',
